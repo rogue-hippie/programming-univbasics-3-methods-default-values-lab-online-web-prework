95b2889c89db9f5780cb54fac46fdd3de79e87be
funny-array-8950 <hodgkins.whitney@gmail.com>
2019-10-13 04:09:53 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,16 @@
 # Your code here
+def meal_choice(veg1, veg2, protein = "meat")
+  puts "What a nutritious meal!"
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+meal_choice("broccoli", "carrots")
+
+def meal_choice(veg1, veg2, protein = "meat")
+  puts "What a nutritious meal!" 
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+
+meal_choice("broccoli", "macaroni")
+
+meal_choice
