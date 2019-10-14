22403b02793f66dae99356321c5e5131553801a1
funny-array-8950 <hodgkins.whitney@gmail.com>
2019-10-14 12:50:11 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,10 +1,21 @@
 # Your code here
 
 
-def meal_choice(veg1, veg2, protein = "meat")
-  puts "What a nutritious meal!"
-  puts "A plate of #{protein} with #{veg1} and #{veg2}."
-  "A plate of #{protein} with #{veg1} and #{veg2}."
-end
+Failures:
 
+  1) retrieve_element_from_index takes in an argument of an array and an index number and returns the element stored at that index
+     Failure/Error: array(index_number)
+
+     NoMethodError:
+       undefined method `array' for #<RSpec::ExampleGroups::RetrieveElementFromIndex:0x00000000016527e0>
+       Did you mean?  Array
+     # ./lib/array_crud.rb:26:in `retrieve_element_from_index'
+     # ./spec/array_crud_spec.rb:50:in `block (2 levels) in <top (required)>'
+
+Finished in 0.00573 seconds (files took 0.34748 seconds to load)
+7 examples, 1 failure
+
+Failed examples:
+
+rspec ./spec/array_crud_spec.rb:47 # retrieve_element_from_index takes in an argument of an array and an index number and returns the element stored at that index
 meal_choice("broccoli", "mashed potatoes")
